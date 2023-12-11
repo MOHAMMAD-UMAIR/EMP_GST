93407642f6bfb383796e4290aa310f3c3a364b55
MOHAMMAD-UMAIR <umair.mohd97@gmail.com>
2023-12-11 13:01:17 +0000

    updated the filling date check

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,20 @@
 import streamlit as st
 from pymongo import MongoClient
 from data_extraction import *
-
+import pandas as pd
 import re
+
+
+# Assuming you have a MongoDB client connected to your database
+#client = pymongo.MongoClient("mongodb://localhost:27017/")
+mongo_uri = "mongodb+srv://umair:umairmongo@cluster0.pgkqn44.mongodb.net/?retryWrites=true&w=majority"
+
+database_name = "GST_Check"
+collection_name = "GST"
+
+client = MongoClient(mongo_uri)
+database = client[database_name]
+collection_GST = database[collection_name]
 
 # MongoDB connection details
 # mongo_uri = "mongodb+srv://umair:umairmongo@cluster0.pgkqn44.mongodb.net/?retryWrites=true&w=majority"
@@ -23,8 +35,66 @@ if st.button("Search"):
         
         if PAN_results:
             st.write(f"Total number of registered GSTs: {len(PAN_results)}")
+            #For top 5 results
+            #top_5=PAN_results[:5]
             st.table(PAN_results)
-            
+            gst_not_in_DB=[]
+            for top in PAN_results:
+                record_exist = search_record_exist(top["gstin"],collection_GST)
+                if record_exist:
+                    print("GST number is in DB")
+                    
+                else:
+                    print("GST is not in DB")
+                    gst_not_in_DB.append(top["gstin"])
+                    
+                    
+            for gst in gst_not_in_DB:
+                status=get_status(gst,PAN_results )
+                if status == 'Active' :
+                    jamku_extract(gst,collection_GST)
+                    print(f"{gst} added in the DB")
+            #if st.button("Get Filing details of each GST"):
+            for top in PAN_results:
+                st.subheader(f"Filing Details for {top['gstin']}")
+                if top["sts"] == "Active" :
+                    print("Generating table")
+                    # Fetch documents where GSTIN is "1234"
+                    query = {"gstin": top["gstin"]}
+                    result = collection_GST.find(query)
+
+                    # Print or process the fetched documents
+                    returns=[]
+                    for document in result:
+                        returns=document["returns"]
+                        
+                        
+                    # Create a dictionary to store records based on rtntype
+                    rtntype_data = {}
+                    for record in returns:
+                        rtntype = record['rtntype']
+                        if rtntype not in rtntype_data:
+                            rtntype_data[rtntype] = []
+                        
+                        # Remove the 'rtntype' key and add the 'check' field
+                        record.pop('rtntype', None)
+                        # dof_day = int(record['dof'].split('/')[0])
+                        # record['check'] = 'Delayed' if dof_day > 15 else 'On time'
+
+                        rtntype_data[rtntype].append(record)
+                
+                    # Display the records in a Streamlit table
+                    for rtntype, records in rtntype_data.items():
+                        st.write(f" Filing Details: {rtntype}")
+                        df = pd.DataFrame(records)
+                        selected_columns = df[['fy', 'taxp','dof','check']]  # List of column names
+                        selected_columns.columns = ['Financial Year', 'Tax Period','Date of Filing', 'Check']
+                        print(selected_columns)
+                        st.table(selected_columns)
+                        
+                else:
+                    st.warning("The GST is not active")        
+                            
         else:
             st.warning("No matching records found.")
     else:
--- utils.py
+++ utils.py
@@ -1,35 +1,158 @@
-import pandas
+import pandas as pd
 import json
 import requests
+from datetime import datetime
 
-def process_jamku(GST):
-    # Example JSON data (dictionary)
+
+# the filling dates of all the GST forms
+
+filing_records = {
+    "GSTR3B": {'July': '20/8/2023',
+               'June': '20/7/2023',
+               'May': '20/06/2023',
+               'April': '24/05/2023',
+               'March': '20/04/2023',
+               'February': '20/03/2023',
+               'January': '20/02/2023',
+               'December': '20/01/2024',
+               'November': '20/12/2023',
+               'October': '20/11/2023',
+               'September': '21/10/2023',
+               'August': '20/9/2023'},
     
-    #is it the right way to do it ?
-    json_data = jamku_extract(GST).text
+    "GSTR1IFF": {'July': '11/8/2023',
+                 'June': '11/7/2023',
+                 'May': '11/06/2023',
+                 'April': '11/05/2023',
+                 'March': '11/04/2023',
+                 'February': '11/03/2023',
+                 'January': '11/02/2023',
+                 'December': '11/01/2024',
+                 'November': '11/12/2023',
+                 'October': '11/11/2023',
+                 'September': '11/10/2023',
+                 'August': '11/9/2023'},
+    
+    "GSTR9": {'2021-2022': '31/12/2022',
+              '2020-2021': '28/02/2022',
+              '2019-2020': '31/03/2021',
+              '2018-2019': '31/12/2020',
+              '2017-2018': '12/02/2020'},
+    
+    "GSTR9C": {'2021-2022': '31/12/2022',
+               '2020-2021': '28/02/2022',
+               '2019-2020': '31/03/2021',
+               '2018-2019': '31/12/2020',
+               '2017-2018': '12/02/2020'},  #17/01/2020
+            
+    
+    "GSTR1": {'July': '11/8/2023',
+              'June': '11/7/2023',
+              'May': '11/06/2023',
+              'April': '11/05/2023',
+              'March': '11/04/2023',
+              'February': '11/03/2023',
+              'January': '11/02/2023',
+              'December': '11/01/2024',
+              'November': '11/12/2023',
+              'October': '11/11/2023',
+              'September': '11/10/2023',
+              'August': '11/9/2023'},
+    
+    "GSTR6": {'July': '13/8/2023',
+              'June': '13/7/2023',
+              'May': '13/06/2023',
+              'April': '13/05/2023',
+              'March': '13/04/2023',
+              'February': '13/03/2023',
+              'January': '13/02/2023',
+              'December': '13/01/2024',
+              'November': '13/12/2023',
+              'October': '13/11/2023',
+              'September': '13/10/2023',
+              'August': '13/9/2023'},
+    
+    "GSTR10": {'2021-2022': '23/04/2023',
+               '2020-2021': '28/02/2022',
+               '2019-2020': '31/03/2021',
+               '2018-2019': '31/12/2020',
+               '2017-2018': '12/02/2021'},
+    
+    "GSTR7": {'July': '10/8/2023',
+              'June': '10/7/2023',
+              'May': '10/06/2023',
+              'April': '10/05/2023',
+              'March': '10/04/2023',
+              'February': '10/03/2023',
+              'January': '10/02/2023',
+              'December': '10/01/2024',
+              'November': '10/12/2023',
+              'October': '10/11/2023',
+              'September': '10/10/2023',
+              'August': '10/9/2023'},
+    
+    "GSTR8": {'July': '10/8/2023',
+        'June': '10/7/2023',
+        'May': '10/06/2023',
+        'April': '10/05/2023',
+        'March': '10/04/2023', 
+        'February': '10/03/2023',
+        'January': '10/02/2023', 
+        'December': '10/01/2024',
+        'November': '10/12/2023',
+        'October': '10/11/2023',
+        'September': '10/10/2023',
+        'August': '10/9/2023'}
+    }
 
 
-    # Convert JSON string to a Python dictionary
-    json_obj = json.loads(json_data)
 
-    # Extract only the "records" data
-    records_data = json_obj["data"]
 
-    # Specify the file path
-    file_path = str(GST)+".json"
 
-    # Save the "records" data to a new JSON file
-    with open(file_path, 'w') as json_file:
-        # Use indent for pretty formatting (optional)
-        json.dump(records_data, json_file, indent=2)
+''' This function checks weather a gst record is already there in the DB
+'''
+def search_record_exist(GST, collection_GST):
+    
+    existing_record = collection_GST.find_one({"gstin": GST})
+    
+    return existing_record
+    
 
-    print(f"Records data has been saved to {file_path}")
+
+
+# def process_jamku(GST,collection_GST,client):
+#     # Example JSON data (dictionary)
+    
+#     #is it the right way to do it ?
+#     json_data = jamku_extract(GST)
+
+    
+#     # Convert JSON string to a Python dictionary
+#     json_obj = json.loads(json_data)
+
+#     # Extract only the "records" data
+#     records_data = json_obj["data"]
+    
+    
+    
+    
+
+#     # # Specify the file path
+#     # file_path = str(GST)+".json"
+
+    # # Save the "records" data to a new JSON file
+    # with open(file_path, 'w') as json_file:
+    #     # Use indent for pretty formatting (optional)
+    #     json.dump(records_data, json_file, indent=2)
+
+    # print(f"Records data has been saved to {file_path}")
     
     
     
     
     
-def jamku_extract(GST):
+def jamku_extract(GST, collection_GST):
+    print(str(GST))
     url = "https://gst-return-status.p.rapidapi.com/free/gstin/"+str(GST)
 
     headers = {
@@ -39,9 +162,75 @@ def jamku_extract(GST):
     }
 
     response = requests.get(url, headers=headers)
+    json_data=response.text
     print(f"Connection established with Jamku API for GST: {GST} ")
     
-    return response
+    # Convert JSON string to a Python dictionary
+    json_obj = json.loads(json_data)
+
+    # Extract only the "records" data
+    records_data = json_obj["data"]
+    
+    updated_at_timestamp = datetime.utcnow().isoformat()
+
+    records_data["updated_at"]=updated_at_timestamp
+    
+    
+    #code for calculating the delay check
+    
+    # Key-Value Dictionary
+
+    # Update the 'check' key and value in the JSON data
+    returns_data = records_data.get("returns", [])
+    for return_obj in returns_data:
+        return_name=return_obj.get("rtntype", "")
+        
+        fy = return_obj.get("fy", "")
+        dof = return_obj.get("dof", "")
+        filing_table=filing_records.get(return_name,"")
+        #filing_date = pd.to_datetime(filing_table.get(fy, ""), format='%dd/%mm/%YYYY')
+        #return_obj["check"] = 'Delayed' if pd.to_datetime(dof, format='%dd/%mm/%YYYY') > filing_date else 'On time'
+        
+        if fy and dof:  # Check if fy and dof are not empty
+            period=return_obj.get("taxp", "")
+            if period == 'Annual':
+                filing_date = pd.to_datetime(filing_table.get(fy, ""), format='%d/%m/%Y')
+                print(f'the filing date is :{filing_date}')
+                filed_date=pd.to_datetime(dof, format='%d/%m/%Y')
+                if filed_date > filing_date:
+                    return_obj["check"] = 'Delayed'
+                    print("greater tahn loop ran")
+                else:
+                    return_obj["check"] = 'On Time'
+                    print('smaller than loop ran')
+                date=pd.to_datetime(dof, format='%d/%m/%Y')
+                print(f'the submitted date is : {date}')
+            else:
+                filing_date = pd.to_datetime(filing_table.get(period, ""), format='%d/%m/%Y')
+                return_obj["check"] = 'Delayed' if pd.to_datetime(dof, format='%d/%m/%Y') > filing_date else 'On time'
+                
+        else:
+            return_obj["check"] = 'Invalid'
+
+    # Create DataFrame
+    df = pd.DataFrame(returns_data)
+
+    # Display the DataFrame
+    #print(df)
+
+    # Update original JSON data with new fields from DataFrame
+    records_data["returns"] = df.to_dict(orient='records')
+
+    # Display the updated JSON data
+    print(records_data)
+    
+    
+    # Insert records into the MongoDB collection
+    collection_GST.insert_one(records_data)
+
+    print(f"Records have been inserted into the GST collection for {GST} ")
+    
+    
 
 
 
@@ -62,14 +251,14 @@ def process_master_india(PAN):
     #print(records_data_filtered)
 
     # Specify the file path
-    file_path = str(PAN)+"_to_GST.json"
+    #file_path = str(PAN)+"_to_GST.json"
 
     # Save the "records" data to a new JSON file
-    with open(file_path, 'w') as json_file:
-        # Use indent for pretty formatting (optional)
-        json.dump(records_data_filtered, json_file, indent=2)
+    # with open(file_path, 'w') as json_file:
+    #     # Use indent for pretty formatting (optional)
+    #     json.dump(records_data_filtered, json_file, indent=2)
 
-    print(f"Records data has been saved to {file_path}")
+    #print(f"Records data has been saved to {file_path}")
     
     return records_data_filtered
     
@@ -102,3 +291,15 @@ def master_india_extract(PAN):
     print(f"Established connection  to the master india website for the PAN:{PAN}")
     
     return master_data
+
+
+# getting gst status
+
+def get_status(gst_value, gst_data):
+    for record in gst_data:
+        if record['gstin'] == gst_value:
+            return record['sts']
+    return 'GST not found'
+
+
+
